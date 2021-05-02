22bcfb4061eab1f0b8a479a9d761b482e7b1d6af
nagendar <nagendar@salatar.in>
2021-05-02 10:31:36 +0530

    Text

---
+++ lib/themes/texts.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Texts {
+  //create your own text style
+  //pass it to theme data under MaterialApp or call it directly
+  static TextStyle body1() {
+    return TextStyle(
+      color: Colors.red,
+      fontSize: 22,
+    );
+  }
+}
